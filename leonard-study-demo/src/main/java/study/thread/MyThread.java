fa7346712d99805973c1b01d0ddb5271bc9c719f
leonardchun <chuncumt@163.com>
2018-05-14 23:11:32 +0800

    线程学习

---
+++ leonard-study-demo/src/main/java/study/thread/MyThread.java
@@ -0,0 +1,29 @@
+package study.thread;
+
+public class MyThread extends Thread {
+
+    private String name;
+
+    public MyThread(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(name + "运行：" + i);
+                sleep(10);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread("A");
+        MyThread thread2 = new MyThread("B");
+
+        thread1.start();
+        thread2.start();
+    }
+}
