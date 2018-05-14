a077fa02902199edec75eb01f19a9f5e7093c1f0
leonardchun <chuncumt@163.com>
2018-05-14 10:56:55 +0800

    zuul网关

---
+++ leonard-zuul-demo/src/test/java/com/leonard/zuul/LeonardZuulDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leonard.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeonardZuulDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
