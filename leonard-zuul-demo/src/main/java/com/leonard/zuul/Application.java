4e6fed799925fdaf8fe56581257fd4167c5f1e58
leonardchun <chuncumt@163.com>
2018-05-14 22:48:34 +0800

    zuul静态路由

--- leonard-zuul-demo/src/main/java/com/leonard/zuul/Application.java
+++ leonard-zuul-demo/src/main/java/com/leonard/zuul/Application.java
@@ -2,10 +2,11 @@ package com.leonard.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
