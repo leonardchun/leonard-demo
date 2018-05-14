5bda04b43daef99cf0509668a2dadcbf138b505d
leonardchun <chuncumt@163.com>
2018-05-14 23:23:11 +0800

    IO

---
+++ leonard-study-demo/src/main/java/study/io/NomallIO.java
@@ -0,0 +1,31 @@
+package study.io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 简单的文件IO 文件的读写
+ */
+public class NomallIO {
+    public static void main(String[] args) {
+        try {
+            FileOutputStream fos = new FileOutputStream("D:/test/test.txt");
+            fos.write("我是测试文件".getBytes());
+            fos.close();
+
+            FileInputStream fis = new FileInputStream("D:/test/read.txt");
+            byte [] bytes = new byte[100];
+            while (fis.read(bytes) != -1){
+                System.out.println(new String(bytes));
+            }
+            fis.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
