734d47acdbdd70eb2cdda8aefab792cb8979ada0
leonardchun <chuncumt@163.com>
2018-07-24 07:47:06 +0800

    tomcat

---
+++ leonard-tomcat-demo/src/main/java/com/leonard/leonardtomcatdemo/main/TomcatMain.java
@@ -0,0 +1,19 @@
+package com.leonard.leonardtomcatdemo.main;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Tomcat 主线程
+ * @author leonard
+ * @date 2018/7/24 7:34
+ */
+@Slf4j
+public class TomcatMain {
+
+    private static final String workspace = "D:\\test\\webapp";
+    public static void main(String[] args) {
+        log.info("*********************开始启动*********************");
+
+        log.info("*********************启动结束*********************");
+    }
+}
---
+++ leonard-tomcat-demo/src/main/java/com/leonard/leonardtomcatdemo/utils/ProjectCheck.java
@@ -0,0 +1,10 @@
+package com.leonard.leonardtomcatdemo.utils;
+
+/**
+ * @author leonard
+ * @date 2018/7/24 7:43
+ */
+public class ProjectCheck {
+
+
+}
