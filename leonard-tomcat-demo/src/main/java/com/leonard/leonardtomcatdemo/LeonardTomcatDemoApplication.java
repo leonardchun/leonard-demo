70b8670d6d553d587e66a8d2d04acfbeed814923
leonardchun <chuncumt@163.com>
2018-07-24 07:31:07 +0800

    tomcat

---
+++ leonard-tomcat-demo/src/main/java/com/leonard/leonardtomcatdemo/LeonardTomcatDemoApplication.java
@@ -0,0 +1,12 @@
+package com.leonard.leonardtomcatdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonardTomcatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonardTomcatDemoApplication.class, args);
+	}
+}
